879514939c6e9076a8e719481a0d89c055bbc617
mrimmoyee <mrimmoyee20@gmail.com>
2025-05-03 23:13:23 +0600

    update export.h
    update export.h

--- mcertikos-lab5/kern/proc/PProc/export.h
+++ mcertikos-lab5/kern/proc/PProc/export.h
@@ -1,12 +1,24 @@
-#ifndef _KERN_THREAD_PTCBINIT_H_
-#define _KERN_THREAD_PTCBINIT_H_
+#ifndef _KERN_PROC_PPROC_H_
+  #define _KERN_PROC_PPROC_H_
 
-#ifdef _KERN_
+  #ifdef _KERN_
 
-unsigned int proc_create(void *elf_addr, unsigned int);
-void proc_start_user(void);
+  #include <lib/types.h>
+  #include <vmm/MPTOp/export.h> // For pgd_t
 
-#endif /* _KERN_ */
+  unsigned int proc_create(void *elf_addr, unsigned int quota);
+  void proc_start_user(void);
+  struct proc *proc_get(unsigned int pid);
 
-#endif /* !_KERN_THREAD_PICBINIT_H_ */
+  struct proc {
+      uint32_t pid; // Process ID
+      uint32_t brk; // Heap break
+      uint32_t heap_start; // Start of heap (e.g., VM_USERLO)
+      pgd_t *pgd; // Page directory
+      int superpage_enabled; // 1 if super pages allowed
+      int contiguous_enabled; // 1 if contiguous allocations allowed
+  };
 
+  #endif /* _KERN_ */
+
+  #endif /* !_KERN_PROC_PPROC_H_ */
