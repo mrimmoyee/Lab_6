743e46a830f7d206d06929f9a253a58e399af07d
mrimmoyee <mrimmoyee20@gmail.com>
2025-05-03 23:15:32 +0600

    pproc.c update
    pproc.c update

--- mcertikos-lab5/kern/proc/PProc/PProc.c
+++ mcertikos-lab5/kern/proc/PProc/PProc.c
@@ -1,43 +1,68 @@
 #include <lib/elf.h>
-#include <lib/debug.h>
-#include <lib/gcc.h>
-#include <lib/seg.h>
-#include <lib/trap.h>
-#include <lib/x86.h>
-#include <pcpu/PCPUIntro/export.h>
+  #include <lib/debug.h>
+  #include <lib/gcc.h>
+  #include <lib/seg.h>
+  #include <lib/trap.h>
+  #include <lib/x86.h>
+  #include <pcpu/PCPUIntro/export.h>
+  #include <vmm/MPTOp/export.h> // For pgd_t
 
-#include "import.h"
+  #include "import.h"
+  #include "export.h" // For struct proc
 
-extern tf_t uctx_pool[NUM_IDS];
+  extern tf_t uctx_pool[NUM_IDS];
+  static struct proc proc_table[NUM_IDS]; // Process table
 
-void proc_start_user(void)
-{
-  unsigned int cur_pid = get_curid();
-  static int first = TRUE;
-  kstack_switch(cur_pid);
-  set_pdir_base(cur_pid);
+  void proc_start_user(void)
+  {
+      unsigned int cur_pid = get_curid();
+      static int first = TRUE;
+      kstack_switch(cur_pid);
+      set_pdir_base(cur_pid);
 
-  trap_return((void *) &uctx_pool[cur_pid]);
-}
+      trap_return((void *) &uctx_pool[cur_pid]);
+  }
 
-unsigned int proc_create(void *elf_addr, unsigned int quota)
-{
-  unsigned int pid, id;
+  unsigned int proc_create(void *elf_addr, unsigned int quota)
+  {
+      unsigned int pid, id;
 
-  id = get_curid();
-  pid = thread_spawn((void *) proc_start_user, id, quota);
+      id = get_curid();
+      pid = thread_spawn((void *) proc_start_user, id, quota);
 
-  elf_load(elf_addr, pid);
+      if (pid == NUM_IDS) {
+          KERN_DEBUG("proc_create: thread_spawn failed\n");
+          return NUM_IDS;
+      }
 
-  uctx_pool[pid].es = CPU_GDT_UDATA | 3;
-  uctx_pool[pid].ds = CPU_GDT_UDATA | 3;
-  uctx_pool[pid].cs = CPU_GDT_UCODE | 3;
-  uctx_pool[pid].ss = CPU_GDT_UDATA | 3;
-  uctx_pool[pid].esp = VM_USERHI;
-  uctx_pool[pid].eflags = FL_IF;
-  uctx_pool[pid].eip = elf_entry(elf_addr);
+      elf_load(elf_addr, pid);
 
-  seg_init_proc(get_pcpu_idx(), pid);
-  
-  return pid;
-}
+      uctx_pool[pid].es = CPU_GDT_UDATA | 3;
+      uctx_pool[pid].ds = CPU_GDT_UDATA | 3;
+      uctx_pool[pid].cs = CPU_GDT_UCODE | 3;
+      uctx_pool[pid].ss = CPU_GDT_UDATA | 3;
+      uctx_pool[pid].esp = VM_USERHI;
+      uctx_pool[pid].eflags = FL_IF;
+      uctx_pool[pid].eip = elf_entry(elf_addr);
+
+      seg_init_proc(get_pcpu_idx(), pid);
+
+      // Initialize proc_table entry
+      proc_table[pid].pid = pid;
+      proc_table[pid].brk = VM_USERLO; // Example heap start
+      proc_table[pid].heap_start = VM_USERLO;
+      proc_table[pid].pgd = get_pdir(pid); // Assuming get_pdir exists
+      proc_table[pid].superpage_enabled = 0;
+      proc_table[pid].contiguous_enabled = 0;
+
+      return pid;
+  }
+
+  struct proc *proc_get(unsigned int pid)
+  {
+      if (pid >= NUM_IDS) {
+          KERN_DEBUG("proc_get: invalid pid %d\n", pid);
+          return NULL;
+      }
+      return &proc_table[pid];
+  }
